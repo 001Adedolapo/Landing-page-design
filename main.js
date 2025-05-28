b3dc3e3c264bb03a8a5c8d9838bfc14df98fb910
001Adedolapo <ebenezeradeniyi2111@gmail.com>
2025-05-28 23:36:07 +0100

    Moved files to docs folder and updated content

--- main.js
+++ main.js
@@ -6,3 +6,21 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
     });
   });
+
+    const backToTopButton = document.querySelector('.back-to-top');
+
+  // Show the button only after scrolling down 100px
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+      backToTopButton.style.display = 'block';
+    } else {
+      backToTopButton.style.display = 'none';
+    }
+  });
+
+  // When button is clicked, scroll to top and hide it
+  backToTopButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    backToTopButton.style.display = 'none';
+  });
+
